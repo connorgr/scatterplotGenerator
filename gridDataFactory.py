7d811704ecd1402d7ad20aa8a31c5cb30e49795c
connor <connor@cs.brown.edu>
2014-03-13 18:16:17 -0400

    Fix bugs

--- gridDataFactory.py
+++ gridDataFactory.py
@@ -7,11 +7,11 @@ import sys
 import time
 
 c = sys.argv[1] # color layout = ['grouped','random']
-m = sys.argv[2] # mark size = [4,6,8,10]
-s = sys.argv[3] # set size = [196, 324, 484]
+m = int(sys.argv[2]) # mark size = [4,6,8,10]
+s = int(sys.argv[3]) # set size = [196, 324, 484]
 slope = sys.argv[4] # slope = ['positive', 'negative']
 q = sys.argv[5] # quadrant = ['tl', 'tr', 'bl', 'br']
-r = sys.argv[6] # repetition = 10
+r = int(sys.argv[6]) # repetition = 10
 
 start = time.clock()
 
@@ -26,9 +26,11 @@ d['repetition'] = r
 elapsed = (time.clock() - start)
 d['timeToMake'] = elapsed
 
+print c, m, q, s, slope, r
 print 'Total elapsed time: ' + str(elapsed)
+print '---------------'
 
-fileName = c + '_' + s + '_' + m + '_' + slope + '_' + q + '_' + r + '.json'
+fileName = c + '_' + str(s) + '_' + str(m) + '_' + slope + '_' + q + '_' + str(r) + '.json'
 filePath = os.getcwd() + '/' + fileName
 with open(filePath, 'w') as fPath:
   json.dump(d, fPath)
---
+++ gridScatterData.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+# c = sys.argv[1] # color layout = ['grouped','random']
+# m = sys.argv[2] # mark size = [4,6,8,10]
+# s = sys.argv[3] # set size = [196, 324, 484]
+# slope = sys.argv[4] # slope = ['positive', 'negative']
+# q = sys.argv[5] # quadrant = ['tl', 'tr', 'bl', 'br']
+# r = sys.argv[6] # repetition = 10
+
+layouts=( "grouped" "random")
+markSizes=( 4 6 8 10 )
+setSizes=( 196 324 484 )
+slopes=( "positive" "negative")
+quads=("tl" "tr" "bl" "br")
+reps=( 1 2 3 4 5 6 7 8 9 10 )
+
+for c in "${layouts[@]}"
+do
+  for m in "${markSizes[@]}"
+  do
+    for s in "${setSizes[@]}"
+    do
+      for slope in "${slopes[@]}"
+      do
+        for q in "${quads[@]}"
+        do
+          for r in "${reps[@]}"
+          do
+            ./gridDataFactory.py $c $m $s $slope $q $r
+          done
+        done
+      done
+    done
+  done
+done
