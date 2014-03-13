7d158c95a8b77f14c877a9fea21a10a24df1fb28
connor <connor@cs.brown.edu>
2014-03-13 17:59:30 -0400

    Revise grid python file

--- gridDataFactory.py
+++ gridDataFactory.py
@@ -1,37 +1,34 @@
+#!/usr/bin/python
+
 import json
 import os
 import scatterData
+import sys
 import time
+
+c = sys.argv[1] # color layout = ['grouped','random']
+m = sys.argv[2] # mark size = [4,6,8,10]
+s = sys.argv[3] # set size = [196, 324, 484]
+slope = sys.argv[4] # slope = ['positive', 'negative']
+q = sys.argv[5] # quadrant = ['tl', 'tr', 'bl', 'br']
+r = sys.argv[6] # repetition = 10
 
 start = time.clock()
 
-colorLayouts = ['grouped','random']
-markSizes = [4,6,8,10]
-quads = ['tl', 'tr', 'bl', 'br']
-slopes = ['positive', 'negative'] # degrees rotating from vertical orientation
-setSizes = [196, 324, 484]
-repetitions = 10
-
-output = []
-for c in colorLayouts:
-  for m in markSizes:
-    for s in setSizes:
-      for slope in slopes:
-        for q in quads:
-          for r in range(repetitions):
-            d = scatterData.makeScatterData(s, slope, q)
-            d['colorLayout'] = c
-            d['markSize'] = m
-            d['quad'] = q
-            d['setSize'] = s
-            d['slope'] = slope
-            c['repetition'] = r
-            output.append(d)
-
-filePath = os.getcwd() + '/clusterdata.json'
-with open(filePath, 'w') as fPath:
-  json.dump(output, fPath)
+d = scatterData.makeScatterData(s, slope, q)
+d['colorLayout'] = c
+d['markSize'] = m
+d['quad'] = q
+d['setSize'] = s
+d['slope'] = slope
+d['repetition'] = r
 
 elapsed = (time.clock() - start)
+d['timeToMake'] = elapsed
 
 print 'Total elapsed time: ' + str(elapsed)
+
+fileName = c + '_' + s + '_' + m + '_' + slope + '_' + q + '_' + r + '.json'
+filePath = os.getcwd() + '/' + fileName
+with open(filePath, 'w') as fPath:
+  json.dump(d, fPath)
