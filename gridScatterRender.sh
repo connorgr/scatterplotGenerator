edb39db13f64d20961967118bf58a494aa5eea22
connor <connor@cs.brown.edu>
2014-03-14 10:40:15 -0400

    Escape dir in gridScatterREnder.sh

--- gridScatterRender.sh
+++ gridScatterRender.sh
@@ -1,7 +1,7 @@
 FILES=/home/connor/scatterplotGenerator/out/*
 for f in $FILES
 do
-  qsub -cwd -N "svg$f" -V -e /home/connor/scatterplotGenerator/gridOut -o /home/connor/scatterplotGenerator/gridOut -l inf node /home/connor/scatterplotGenerator/gridRenderScatter.js --outdir=/home/connor/scatterplotGenerator/svg --json=$f
+  qsub -cwd -N "svg${f/\//-}" -V -e /home/connor/scatterplotGenerator/gridOut -o /home/connor/scatterplotGenerator/gridOut -l inf node /home/connor/scatterplotGenerator/gridRenderScatter.js --outdir=/home/connor/scatterplotGenerator/svg --json=$f
 done
 
 #qsub -cwd -N "vizDataGenerator$c$m$s$slope$q$r" -V -e /home/connor/scatterplotGenerator/gridOut -o /home/connor/scatterplotGenerator/gridOut -l inf /home/connor/scatterplotGenerator/gridReady.py $c $m $s $slope $q $r
