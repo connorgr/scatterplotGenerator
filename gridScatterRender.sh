5c0b0edc8413d067cc1e0481f955c44dbe20a53b
connor <connor@cs.brown.edu>
2014-03-14 10:34:27 -0400

    Make gridScatterRender.sh

--- gridScatterRender.sh
+++ gridScatterRender.sh
@@ -1,4 +1,4 @@
-FILES = /home/connor/scatterplotGenerator/gridOut/*
+FILES= /home/connor/scatterplotGenerator/gridOut/*
 for f in $FILES
 do
   qsub -cwd -N "svg$f" -V -e /home/connor/scatterplotGenerator/gridOut -o /home/connor/scatterplotGenerator/gridOut -l inf node /home/connor/scatterplotGenerator/gridRenderScatter.js --outdir=/home/connor/scatterplotGenerator/svg --json=$f
