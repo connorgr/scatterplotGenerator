6b8999efbd68b37db2bc624dc785f20e1bc4adda
connor <connor@cs.brown.edu>
2014-03-14 10:48:58 -0400

    Adapt grid scripts

--- submitScatterRender.sh
+++ submitScatterRender.sh
@@ -1,3 +1,5 @@
 NAME=$1
 FILE=$2
+echo $NAME
+echo $FILE
 qsub -cwd -N $NAME -V -e /home/connor/scatterplotGenerator/gridOut -o /home/connor/scatterplotGenerator/gridOut -l inf /local/bin//node /home/connor/scatterplotGenerator/gridRenderScatter.js --outdir=/home/connor/scatterplotGenerator/svg --json=$FILE
